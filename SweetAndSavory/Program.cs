e8cb1e0cd57981468cf8e9206c39074d5a3e8658
Bradbuchholz <bradbuchholz@gmail.com>
2022-08-28 10:37:36 -0700

    debugging

---
+++ SweetAndSavory/Program.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SweetAndSavory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .ConfigureLogging(
+                    logBuilder =>
+                    {
+                        logBuilder.ClearProviders(); // removes all providers from LoggerFactory
+                        logBuilder.AddConsole();
+                    }
+                )
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
